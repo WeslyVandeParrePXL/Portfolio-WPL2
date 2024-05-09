e7e1e9aefc02286cd8627d753db09e73bd3809ff
Wesly <wesly.VandeParre@student.pxl.be>
2024-05-09 15:13:16 +0200

    first commit

---
+++ wpl2-Portfolio-WeslyVandeParrePXL/README.md
@@ -0,0 +1,11 @@
+# wpl1-2324-start
+
+# Naam
+Wesly Van de Parre
+
+# Studenten-nummer
+12101876
+
+# Klas
+1DVOa + 2DVOb
+
---
+++ wpl2-Portfolio-WeslyVandeParrePXL/script.js
@@ -0,0 +1,17 @@
+function showProject() {
+    const projectSections = document.querySelectorAll('.projectSection');
+    projectSections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    const selectedProjectId = document.getElementById('projectDropdown').value;
+    const selectedProject = document.getElementById(selectedProjectId);
+    if (selectedProject) {
+        selectedProject.style.display = 'block';
+    }
+}
+
+showProject();
+
+
+// Voor deze code heb ik wat hulp gekregen via Stack overflow en van junior collega's.
